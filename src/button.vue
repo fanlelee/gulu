a3d7a3b7fc8bb6478a9c7616415ff810ee23bf04
fanlelee <529378687@qq.com>
2019-12-20 16:22:15 +0800

    将button.js改为./src/button.vue单文件组件 加入parcel，修改package.json配置

---
+++ src/button.vue
@@ -0,0 +1,26 @@
+<template>
+    <button class="g-button">按钮</button>
+</template>
+<script>
+    export default {}
+</script>
+<style lang="scss">
+    .g-button {
+        font-size: var(--font-size);
+        height: var(--button-height);
+        padding: 0 2em;
+        background-color: var(--button-bg);
+        border-radius: var(--button-radius);
+        border-color: var(--button-border-color);
+        color: var(--button-color);
+        &:hover {
+            border-color: var(--button-hover-color);
+        }
+        &:active {
+            background-color: var(--button-active-bg);
+        }
+        &:focus {
+            outline: none;
+        }
+    }
+</style>
