3fffcdf16cba1fd2c19ed0f789af94edf338a526
fanlelee <529378687@qq.com>
2019-12-22 17:25:18 +0800

    新建button-group

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import Button from './button'
 import Icon from './icon'
+import  ButtonGroup from  './button-group'
 
 
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
+Vue.component('button-group', ButtonGroup)
 
 new Vue({
     el: "#app",
---
+++ src/button-group.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="g-button-group">
+        <slot></slot>
+    </div>
+</template>
+<script>
+
+</script>
+<style lang="scss">
+    .g-button-group {
+        display: inline-flex;
+        vertical-align: middle;
+        > .g-button {
+            border-radius: 0;
+            /*&:not(:first-child){*/
+            /*    border-left: none;*/
+            /*}*/
+            margin-left: -1px;
+            &:first-child {
+                border-top-left-radius: var(--button-radius);
+                border-bottom-left-radius: var(--button-radius);
+            }
+            &:last-child{
+                border-top-right-radius: var(--button-radius);
+                border-bottom-right-radius: var(--button-radius);
+            }
+
+            &:hover{
+            position: relative;
+            z-index: 1;
+            }
+        }
+    }
+</style>
