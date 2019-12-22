dd3f33ae0865ff77ee2faa22d8eb11681c87308f
fanlelee <529378687@qq.com>
2019-12-22 16:11:15 +0800

    添加icon=>loading，及其样式

--- src/button.vue
+++ src/button.vue
@@ -1,6 +1,7 @@
 <template>
     <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
-        <g-icon v-if="icon" :name="icon"/>
+        <g-icon v-if="icon" :name="icon"></g-icon>
+        <g-icon class="loading" name="loading"></g-icon>
         <div class="content">
             <slot></slot>
         </div>
@@ -23,6 +24,14 @@
     }
 </script>
 <style lang="scss">
+    @keyframes spin {
+        0%{
+            transform: rotate(0deg);
+        }
+        100%{
+            transform: rotate(360deg);
+        }
+    }
     .g-button {
         font-size: var(--font-size);
         height: var(--button-height);
@@ -68,6 +77,10 @@
                 margin-left: .3em;
                 margin-right: 0;
             }
+        }
+
+        .loading {
+            animation: spin 1s infinite linear;
         }
     }
 </style>
