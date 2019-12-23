89cbf808c09221f5fd9ff518eb7aff5751acd3cc
fanlelee <529378687@qq.com>
2019-12-23 12:32:45 +0800

    使用chai.expect做测试用例 添加两个测试用例 loading和iconPosition

--- src/app.js
+++ src/app.js
@@ -20,15 +20,56 @@ new Vue({
 //单元测试
 import chai from 'chai'
 const expect = chai.expect
+
+//icon测试用例
 {
     const Constructor = Vue.extend(Button)
-    const button = new Constructor({
+    const vm = new Constructor({
         propsData: {
             icon: 'settings'
         }
     })
-    button.$mount('#test')
-    let useElement =  button.$el.querySelector('use')
-    console.log(useElement)
+    vm.$mount('#test')
+    let useElement =  vm.$el.querySelector('use')
     expect(useElement.getAttribute('xlink:href')).to.equal('#i-settings')
+
+    vm.$el.remove()
+    vm.$destroy()
+}
+
+//loading测试用例
+{
+    const Constructor = Vue.extend(Button)
+    let vm = new Constructor({
+        propsData: {
+            icon: 'settings',
+            loading: true
+        }
+    }).$mount()//可以不显示在页面，就放在内存中
+
+    let href = vm.$el.querySelector('use').getAttribute("xlink:href")
+    expect(href).to.eq('#i-loading')
+
+    vm.$el.remove()
+    vm.$destroy()
+}
+
+//iconPosition测试用例
+{
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    let vm = new Constructor({
+        propsData: {
+            icon: 'settings',
+            iconPosition: 'right'
+        }
+    }).$mount(div)//因为要断言css，所有要让它在页面显示出来
+
+    let svg = vm.$el.querySelector('svg')
+    let {order} = window.getComputedStyle(svg) //所有css属性结果都是字符串
+
+    expect(order).to.eq('2')//注意用字符串来断言
+    vm.$el.remove()
+    vm.$destroy()
 }
