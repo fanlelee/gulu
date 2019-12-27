8347717eebf8590e27933219ab5f55c1c6abe5d3
fanlelee <529378687@qq.com>
2019-12-27 18:49:27 +0800

    完成row和col的雏形

---
+++ src/col.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="col" :class="[`col-${span}`]">
+    </div>
+</template>
+<script>
+    export default {
+        props: {
+            span:{
+                type: [Number,String]
+            }
+        }
+    }
+
+</script>
+<style lang="scss" scoped>
+    .col {
+        height: 100px;
+        width: 100%;
+        background-color: grey;
+        border: 1px solid red;
+
+        $class: col-;
+        @for $n from 1 through 24 {
+            &.#{$class}#{$n} {
+                width: ($n/24)*100%;
+            }
+        }
+    }
+</style>
