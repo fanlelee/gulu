d53cda75a92fdf5b86f5014ac2558c38fb25705c
fanlelee <529378687@qq.com>
2019-12-27 23:00:52 +0800

    给网格添加空隙

--- src/col.vue
+++ src/col.vue
@@ -1,28 +1,47 @@
 <template>
-    <div class="col" :class="[`col-${span}`]">
+    <div class="col" :class="[span &&`col-${span}`, offset && `offset-${offset}`]"
+         :style="gutter && {paddingLeft: gutter/2+'px', paddingRight: gutter/2+'px'}">
+        <div class="content"></div>
     </div>
 </template>
 <script>
     export default {
         props: {
-            span:{
-                type: [Number,String]
+            span: {
+                type: [Number, String]
+            },
+            offset: {
+                type: [Number, String]
+            }
+        },
+        data() {
+            return {
+                gutter: 0
             }
         }
     }
-
 </script>
 <style lang="scss" scoped>
     .col {
         height: 100px;
         width: 100%;
-        background-color: grey;
-        border: 1px solid red;
+
+        > .content {
+            border: 1px solid green;
+            height: inherit;
+        }
 
         $class: col-;
         @for $n from 1 through 24 {
             &.#{$class}#{$n} {
                 width: ($n/24)*100%;
+            }
+        }
+
+        $class: offset-;
+        @for $n from 1 through 24 {
+            &.#{$class}#{$n} {
+                margin-left: ($n/24)*100%;
             }
         }
     }
--- src/row.vue
+++ src/row.vue
@@ -1,8 +1,20 @@
 <template>
-    <div class="row">
+    <div class="row" :style="{marginLeft: -gutter/2+'px', marginRight: -gutter/2+'px'}">
         <slot></slot>
     </div>
 </template>
+<script>
+    export default {
+        props: {
+            gutter: [Number, String]
+        },
+        mounted() {
+            this.$children.forEach( (vm) => {
+                vm.gutter = this.gutter
+            } )
+        }
+    }
+</script>
 <style lang="scss" scoped>
     .row {
         display: flex;
