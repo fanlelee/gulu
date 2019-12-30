2949e62245112897baff8b9c94296fb3452f74e4
fanlelee <529378687@qq.com>
2019-12-30 14:55:06 +0800

    初步实现响应式

--- src/col.vue
+++ src/col.vue
@@ -5,6 +5,15 @@
     </div>
 </template>
 <script>
+    let validator = (value) => {
+        let valid = true
+        Object.keys(value).forEach( (key) => {
+            if(!['span', 'offset'].includes(key)){
+                valid = false
+            }
+        } )
+        return valid
+    }
     export default {
         props: {
             span: {
@@ -12,7 +21,11 @@
             },
             offset: {
                 type: [Number, String]
-            }
+            },
+            phone: {type: Object, validator},
+            pad: {type: Object, validator},
+            pc: {type: Object, validator},
+            widePc: {type: Object, validator}
         },
         data() {
             return {
@@ -21,8 +34,14 @@
         },
         computed: {
             colClass(){
-                let {span, offset} = this
-                return [span &&`col-${span}`, offset && `offset-${offset}`]
+                let {span, offset, phone, pad, pc, widePc} = this
+                return [span &&`col-${span}`,
+                    offset && `offset-${offset}`,
+                    phone && `col-phone-${phone.span}`,
+                    pad && `col-pad-${pad.span}`,
+                    pc && `col-pc-${pc.span}`,
+                    widePc && `col-wide-pc-${widePc.span}`
+                ]
             },
             colStyle(){
                 let {gutter} = this
@@ -47,6 +66,67 @@
         @for $n from 1 through 24 {
             &.#{$class}#{$n} {
                 margin-left: ($n/24)*100%;
+            }
+        }
+        
+        @media (max-width: 576px) {
+            $class: col-phone-;
+            @for $n from 1 through 24 {
+                &.#{$class}#{$n} {
+                    width: ($n/24)*100%;
+                }
+            }
+
+            $class: offset-phone-;
+            @for $n from 1 through 24 {
+                &.#{$class}#{$n} {
+                    margin-left: ($n/24)*100%;
+                }
+            }
+        }
+        @media (min-width: 576px) and (max-width: 768px) {
+            $class: col-pad-;
+            @for $n from 1 through 24 {
+                &.#{$class}#{$n} {
+                    width: ($n/24)*100%;
+                }
+            }
+
+            $class: offset-pad-;
+            @for $n from 1 through 24 {
+                &.#{$class}#{$n} {
+                    margin-left: ($n/24)*100%;
+                }
+            }
+        }
+        @media (min-width: 768px) and (max-width: 1200px) {
+            $class: col-pc-;
+            @for $n from 1 through 24 {
+                &.#{$class}#{$n} {
+                    width: ($n/24)*100%;
+                }
+            }
+
+            $class: offset-pc-;
+            @for $n from 1 through 24 {
+                &.#{$class}#{$n} {
+                    margin-left: ($n/24)*100%;
+                }
+            }
+        }
+        @media (min-width: 1200px) and (max-width: 1600px) {
+            $class: col-wide-pc-;
+            @for $n from 1 through 24 {
+                &.#{$class}#{$n} {
+                    width: ($n/24)*100%;
+                }
+            }
+
+            $class: offset-wide-pc-;
+            @for $n from 1 through 24 {
+                &.#{$class}#{$n} {
+                    margin-left: ($n/24)*100%;
+                }
             }
         }
     }
