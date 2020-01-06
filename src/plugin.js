7ff51ef065f9d8c26291f136a5f890b8f67f1cba
fanlelee <529378687@qq.com>
2020-01-06 22:59:04 +0800

    修改currentToast的bug

--- src/plugin.js
+++ src/plugin.js
@@ -1,14 +1,31 @@
 import Toast from './toast'
+
+let currentToast
 export default {
-    install(Vue, options){
+    install(Vue, options) {
         Vue.prototype.$toast = function (message, toastOptions) {
-            let Constructor = Vue.extend(Toast)
-            let toast = new Constructor({
-                propsData: toastOptions
+            if (currentToast) {
+                currentToast.close()
+            }
+            currentToast = creatToast({
+                Vue,
+                message,
+                propsData: toastOptions,
+                onClose: () => {
+                    currentToast = null
+                }
             })
-            toast.$slots.default = [message]
-            toast.$mount()
-            document.body.appendChild(toast.$el)
         }
     }
+}
+
+/* Helpers */
+function creatToast({Vue, message, propsData, onClose}) {
+    let Constructor = Vue.extend(Toast)
+    let toast = new Constructor({propsData})
+    toast.$slots.default = [message]
+    toast.$mount()
+    document.body.appendChild(toast.$el)
+    toast.$on('close', onClose)
+    return toast
 }
