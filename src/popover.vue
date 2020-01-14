cbbaa2f010a889d777ca32adbffaecdfe640b1f3
fanlelee <529378687@qq.com>
2020-01-14 11:36:14 +0800

    点击document,悬浮消失; 解决外层元素为overflow:hidden的bug; 解决窗口滚动时悬浮元素定位bug；

--- src/popover.vue
+++ src/popover.vue
@@ -1,9 +1,11 @@
 <template>
-    <div class="popover" @click="xxx">
-        <div class="content-wrapper" v-if="visible">
+    <div class="popover" @click.stop="xxx">
+        <div ref="contentWrapper" class="content-wrapper" v-if="visible" @click.stop>
             <slot name="content"></slot>
         </div>
-        <slot></slot>
+        <span ref="triggerWrapper">
+            <slot></slot>
+        </span>
     </div>
 </template>
 <script>
@@ -17,23 +19,36 @@
         methods: {
             xxx() {
                 this.visible = !this.visible
+                if (this.visible === true) {
+                    this.$nextTick(() => {
+                        document.body.appendChild(this.$refs.contentWrapper)
+                        const {width,height,top,left} = this.$refs.triggerWrapper.getBoundingClientRect()
+                        this.$refs.contentWrapper.style.top = top+window.scrollY+'px'
+                        this.$refs.contentWrapper.style.left = left+window.scrollX+'px'
+                        let eventHandler = () => {
+                            this.visible = false
+                            document.removeEventListener('click', eventHandler)
+                        }
+                        document.addEventListener('click', eventHandler)
+                    })
+                }
             }
-        }
+        },
+        mounted() {
 
+        }
     }
+
 </script>
 
 <style lang="scss" scoped>
     .popover {
         display: inline-block;
         position: relative;
-
-        > .content-wrapper {
-            position: absolute;
-            left: 0;
-            bottom: 100%;
-            border: 1px solid #000;
-            box-shadow: 0 0 3px 1px rgba(0, 0, 0, .5);
-        }
+    }
+    .content-wrapper {
+        position: absolute;
+        box-shadow: 0 0 3px 1px rgba(0, 0, 0, .5);
+        transform: translateY(-100%);
     }
 </style>
