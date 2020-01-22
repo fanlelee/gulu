03547a9ead597e27b640c093373655ab83669425
fanlelee <529378687@qq.com>
2020-01-22 15:39:11 +0800

    创建cascader并将级联数据渲染到cascader

--- src/app.js
+++ src/app.js
@@ -20,6 +20,7 @@ import TabsContent from './tabs-content'
 import Popover from './popover'
 import Collapse from './collapse'
 import CollapseItem from './collapse-item'
+import Cascader from './cascader'
 
 
 Vue.component('g-button', Button)
@@ -44,13 +45,52 @@ Vue.component('g-tabs-content', TabsContent)
 Vue.component('g-popover', Popover)
 Vue.component('g-collapse', Collapse)
 Vue.component('g-collapse-item', CollapseItem)
+Vue.component('g-cascader', Cascader)
 
 new Vue({
     el: "#app",
     data: {
-        selectedTab:['2','3']
+        sources: [{
+            name: '四川',
+            children: [{
+                name: '成都',
+                children: [
+                    {name: '锦江区'},
+                    {name:'青羊区'},
+                    {name:'金牛区'},
+                    {name:'武侯区'}
+                ]
+            }, {
+                name: '自贡',
+                children: [
+                    {name: '自流井区'},
+                    {name:'大安区'},
+                    {name:'沿滩区'}
+                ]
+            }]
+        },{
+            name: '浙江',
+            children: [{
+                name: '杭州',
+                children: [
+                    {name:'上城区'},
+                    {name:'下城区'},
+                    {name:'江干区'},
+                    {name:'拱墅区'},
+                    {name:'西湖区'}
+                ]
+            },{
+                name:'宁波',
+                children: [
+                    {name:'海曙区'},
+                    {name:'江北区'},
+                    {name:'北仑区'},
+                    {name:'镇海区'}
+                ]
+            }]
+        }]
+
     },
-    methods: {
-    }
+    methods: {}
 })
 
---
+++ src/cascader.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="cascader">
+        <div class="trigger">
+            <slot></slot>
+        </div>
+        <div class="popover" v-if="source">
+            <div v-for="item in source">
+                <cascader-item :source-item="item"></cascader-item>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import CascaderItem from './cascader-item'
+    export default {
+        name: "GuluCascader",
+        components:{
+            CascaderItem
+        },
+        props:{
+            source:{
+                type:Array
+            }}
+    }
+</script>
+
+<style lang="scss" scoped>
+    .cascader {
+    }
+</style>
