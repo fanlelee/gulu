b7bcbd86daecae233641558f9939419df6708a51
fanlelee <529378687@qq.com>
2020-02-14 18:37:55 +0800

    改用vue-cli构建项目

--- README.md
+++ README.md
@@ -1,62 +1,24 @@
-# Vue UI 组件
-持续集成标
-[![Build Status](https://travis-ci.org/fanlelee/gulu.svg?branch=master)](https://travis-ci.org/fanlelee/gulu)
+# hello
 
-## 介绍
-这是用vue做的一个UI框架，希望对你有用。
+## Project setup
+```
+yarn install
+```
 
-## 开始使用
+### Compiles and hot-reloads for development
+```
+yarn serve
+```
 
-1. 添加 css 样式
+### Compiles and minifies for production
+```
+yarn build
+```
 
-    使用本框架前，请在css中开启 border-box
-    ```css
-    *, *::before, *::after {
-        box-sizing: border-box;
-    }
-    ```
-    你还需要设置默认颜色等变量（后续会改为scss变量）
-    ```css
-        :root {
-            --button-height: 32px;
-            --font-size: 14px;
-            --button-bg: #fff;
-            --button-radius: 4px;
-            --button-color: #999;
-            --button-border-color: #999;
-            --button-hover-color: #666;
-            --button-active-bg: #eee;
-        }
-    ```
-    IE 15 及以上浏览器都支持此样式。
-2. 安装 
-    `npm i --save fanlelee-t-1-1`
-3. 引入
-    ```html
-    <template>
-      <div id="app">
-        <g-button>使用</g-button>
-      </div>
-    </template>
-    <script>
-    import { Button } from 'fanlelee-t-1-1'
-    import  'fanlelee-t-1-1/dist/index.css'
-    
-    export default {
-      name: 'app',
-      components: {
-        'g-button': Button
-      }
-    }
-    </script>
-    ```
+### Run your unit tests
+```
+yarn test:unit
+```
 
-## 文档
-
-## 提问
-
-## 变更记录
-
-## 联系方式
-
-## 贡献代码
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/demo.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="demo">
+        <p>111</p>
+        <g-cascader :source="sources" popover-height="200px"></g-cascader>
+        <p>222</p>
+    </div>
+</template>
+
+<script>
+    import Cascader from './cascader.vue'
+
+    export default {
+        name: "demo",
+        components: {
+            'g-cascader': Cascader
+        },
+        data() {
+            return {
+                sources: [{
+                    name: '四川',
+                    children: [{
+                        name: '成都',
+                        children: [
+                            {name: '锦江区'},
+                            {name: '青羊区'},
+                            {name: '金牛区'},
+                            {name: '武侯区'}
+                        ]
+                    }, {
+                        name: '自贡',
+                        children: [
+                            {name: '自流井区'},
+                            {name: '大安区'},
+                            {name: '沿滩区'}
+                        ]
+                    }]
+                }, {
+                    name: '浙江',
+                    children: [{
+                        name: '杭州',
+                        children: [
+                            {name: '上城区'},
+                            {name: '下城区'},
+                            {name: '江干区'},
+                            {name: '拱墅区'},
+                            {name: '西湖区'}
+                        ]
+                    }, {
+                        name: '宁波',
+                        children: [
+                            {name: '海曙区'},
+                            {name: '江北区'},
+                            {name: '北仑区'},
+                            {name: '镇海区'}
+                        ]
+                    }]
+                }]
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    * {margin: 0;padding: 0;box-sizing: border-box;}
+
+    .demo {
+        font-size: 14px;
+    }
+</style>
