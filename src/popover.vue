30c8415eb85d1ae273ac74f6a8cabdef5b89501c
fanlelee <529378687@qq.com>
2020-03-20 23:16:31 +0800

    把destroy改为beforeDestroy

--- src/popover.vue
+++ src/popover.vue
@@ -50,7 +50,7 @@
                 this.$refs.popover.addEventListener('mouseleave', this.close)
             }
         },
-        destroyed() {
+        beforeDestroy() {
             if (this.trigger === 'click') {
                 this.$refs.popover.removeEventListener('click', this.onClick)
             } else if (this.trigger === 'hover') {
