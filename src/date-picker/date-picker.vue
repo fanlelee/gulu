9094b18419a1010116e7aa643e30bb682eda8744
fanlelee <529378687@qq.com>
2020-04-27 16:38:54 +0800

    选择年或月的滚动效果

--- src/date-picker/date-picker.vue
+++ src/date-picker/date-picker.vue
@@ -5,7 +5,7 @@
                 <g-input :value="formattedDate"></g-input>
             </template>
             <template v-slot:content>
-                <div :class="c('pop')">
+                <div :class="c('pop')" @selectstart.prevent>
                     <div :class="c('nav')">
                         <span :class="c('nav-icon')" @click="onClickPreYear">
                             <g-icon name="leftleft"></g-icon>
@@ -14,8 +14,7 @@
                             <g-icon name="left"></g-icon>
                         </span>
                         <span :class="c('yearAndMonth')">
-                            <span @click="onClickYear">{{displayYearAndMonth.year}}年</span>
-                            <span @click="onClickMonth">{{displayYearAndMonth.month+1}}月</span>
+                            <span @click="onClickMonth">{{displayYearAndMonth.year}}年{{displayYearAndMonth.month+1}}月</span>
                         </span>
                         <span :class="c('nav-icon')" @click="onClickNextMonth">
                             <g-icon name="right"></g-icon>
@@ -25,23 +24,63 @@
                         </span>
                     </div>
                     <div class="gulu-date-picker-panel">
-                        <div v-if="mode==='years'" class="gulu-date-picker-content">
-                            <span>年</span>
-                        </div>
-                        <div v-else-if="mode==='months'" class="gulu-date-picker-content">
-                            <span>月</span>
-                        </div>
-                        <div v-else class="gulu-date-picker-content">
-                            <div :class="c('weeks')">
-                                <span :class="c('week-item')" v-for="i in [1,2,3,4,5,6,0]" :key="i">{{weeks[i]}}</span>
-                            </div>
-                            <div :class="c('row')" v-for="i in helper.range(1,7)" :key="i">
+                        <div class="gulu-date-picker-content">
+                            <template v-if="mode==='months'">
+                                <div :class="c('selectYearAndMonth')">
+                                    <select :value="displayYearAndMonth.year" name=""  @change="onChangeSelectYear">
+                                        <option value="2001">2001</option>
+                                        <option value="2002">2002</option>
+                                        <option value="2003">2003</option>
+                                        <option value="2004">2004</option>
+                                        <option value="2005">2005</option>
+                                        <option value="2006">2006</option>
+                                        <option value="2007">2007</option>
+                                        <option value="2008">2008</option>
+                                        <option value="2009">2009</option>
+                                        <option value="2010">2010</option>
+                                        <option value="2011">2011</option>
+                                        <option value="2012">2012</option>
+                                        <option value="2013">2013</option>
+                                        <option value="2014">2014</option>
+                                        <option value="2015">2015</option>
+                                        <option value="2016">2016</option>
+                                        <option value="2017">2017</option>
+                                        <option value="2018">2018</option>
+                                        <option value="2019">2019</option>
+                                        <option value="2020">2020</option>
+                                        <option value="2021">2021</option>
+                                        <option value="2022">2022</option>
+                                    </select>
+                                    <select :value="displayYearAndMonth.month" @change="onChangeSelectMonth">
+                                        <option value="0">1</option>
+                                        <option value="1">2</option>
+                                        <option value="2">3</option>
+                                        <option value="3">4</option>
+                                        <option value="4">5</option>
+                                        <option value="5">6</option>
+                                        <option value="6">7</option>
+                                        <option value="7">8</option>
+                                        <option value="8">9</option>
+                                        <option value="9">10</option>
+                                        <option value="10">11</option>
+                                        <option value="11">12</option>
+                                    </select>
+                                </div>
+                            </template>
+
+                            <template v-else>
+                                <div :class="c('weeks')">
+                                    <span :class="c('week-item')" v-for="i in [1,2,3,4,5,6,0]"
+                                          :key="i">{{weeks[i]}}</span>
+                                </div>
+                                <div :class="c('row')" v-for="i in helper.range(1,7)" :key="i">
                                 <span :class="[c('cell'),{'inCurrentMonth':isInDisplayMonth(getVisibleDay(i,j))}]"
                                       v-for="j in helper.range(1,8)" :key="j"
                                       @click="onClickDay(getVisibleDay(i,j))">
                                     {{getVisibleDay(i,j).getDate()}}
                                 </span>
-                            </div>
+                                </div>
+                            </template>
                         </div>
                     </div>
                 </div>
@@ -95,6 +134,12 @@
             this.popoverContainer = this.$refs.datePickerWrapper
         },
         methods: {
+            onChangeSelectYear(e){
+                this.displayYearAndMonth.year = e.target.value-0
+            },
+            onChangeSelectMonth(e){
+                this.displayYearAndMonth.month = e.target.value-0
+            },
             isInDisplayMonth(date) {
                 let [year1, month1] = helper.yearMonthDay(date)
                 return year1 === this.displayYearAndMonth.year && month1 === this.displayYearAndMonth.month
@@ -110,11 +155,12 @@
             c(className) {
                 return `gulu-date-picker-${className}`
             },
-            onClickYear() {
-                this.mode = 'years'
-            },
             onClickMonth() {
-                this.mode = 'months'
+                if (this.mode === 'months') {
+                    this.mode = 'days'
+                } else {
+                    this.mode = 'months'
+                }
             },
             onClickPreYear() {
                 if (this.displayYearAndMonth.year <= 0) return
@@ -161,6 +207,14 @@
             &.inCurrentMonth {
                 color: black;
             }
+        }
+
+        &-selectYearAndMonth {
+            width: 224px;
+            height: 224px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
         }
 
         /deep/ .gulu-popover-content-wrapper {
